3280362e78ee08f245f54d61cb61e4f4693efb0d
Miguel Gimenez <gimenezgimenezmiguel@gmail.com>
2024-05-03 15:20:00 +0200

    test: :test_tube: fix user tests

--- src/__tests__/integration/user.integration.test.ts
+++ src/__tests__/integration/user.integration.test.ts
@@ -1,8 +1,6 @@
 import request from 'supertest';
 import bcrypt from 'bcryptjs';
 
-
-
 import express from 'express';
 
 import { Model } from 'mongoose';
@@ -10,37 +8,27 @@ import { Model } from 'mongoose';
 import { IUser } from '../../models/user';
 import App from '../../app';
 import DIContainer from '../../di/diContainer';
-import PostRouter from '../../api/posts/PostRouter';
-import UserRouter from '../../api/users/UserRouter';
+
+import UserRouter from '../../api/user/UserRouter';
 import DI_TYPES from '../../di/DITypes';
 import IDatabase from '../../database/interfaces/IDatabase';
 
 import availableModels from '../../models';
 
-// import { MONGO_URI, PORT } from '../../config';
-// const PORT = process.env.PORT
-
-
-
 describe('User Routes', () => {
-  let app: express.Application
+  let app: express.Application;
 
-  let User: Model<IUser>
+  let User: Model<IUser>;
   beforeAll(async () => {
+    app = await new App().config([new UserRouter()]);
+    const database = DIContainer.get<IDatabase>(DI_TYPES.Database);
 
-    app = await new App().config([
-      new PostRouter(),
-      new UserRouter()
-    ]);
-    const database = DIContainer.get<IDatabase>(DI_TYPES.Database)
-
-    User = database.getModel(availableModels.user)
-
-  })
+    User = database.getModel(availableModels.user);
+  });
   afterAll(async () => {
-    const database = DIContainer.get<IDatabase>(DI_TYPES.Database)
-    await database.disconnect()
-  })
+    const database = DIContainer.get<IDatabase>(DI_TYPES.Database);
+    await database.disconnect();
+  });
 
   describe('Login Tests', () => {
     beforeEach(async () => {
@@ -48,37 +36,56 @@ describe('User Routes', () => {
     });
 
     it('should fail with invalid data', async () => {
-      const response = await request(app).post('/api/users/login').send({ email: 'invalid', password: 'password' });
+      const response = await request(app)
+        .post('/api/user/login')
+        .send({ email: 'invalid', password: 'password' });
       expect(response.statusCode).toBe(400);
       expect(response.body.name).toBe('ZodValidationError');
     });
 
     it('should fail with non-existing user', async () => {
-      const response = await request(app).post('/api/users/login').send({ email: 'nonexistent@example.com', password: 'password123' });
+      const response = await request(app)
+        .post('/api/user/login')
+        .send({ email: 'nonexistent@example.com', password: 'password123' });
       expect(response.statusCode).toBe(401);
       expect(response.body.message).toBe('Invalid Credentials');
     });
 
     it('should fail with correct user but wrong password', async () => {
-      const user = new User({ email: 'test@example.com', password: bcrypt.hashSync('correctPassword', 10) });
+      const user = new User({
+        email: 'test@example.com',
+        password: bcrypt.hashSync('correctPassword', 10),
+      });
       await user.save();
-      const response = await request(app).post('/api/users/login').send({ email: 'test@example.com', password: 'wrongPassword' });
+      const response = await request(app)
+        .post('/api/user/login')
+        .send({ email: 'test@example.com', password: 'wrongPassword' });
       expect(response.statusCode).toBe(401);
       expect(response.body.message).toBe('Invalid Credentials');
     });
 
     it('should succeed with correct user and password', async () => {
-      const user = new User({ email: 'test@example.com', password: bcrypt.hashSync('correctPassword', 10) });
+      const user = new User({
+        email: 'test@example.com',
+        password: bcrypt.hashSync('correctPassword', 10),
+      });
       await user.save();
-      const response = await request(app).post('/api/users/login').send({ email: 'test@example.com', password: 'correctPassword' });
+      const response = await request(app)
+        .post('/api/user/login')
+        .send({ email: 'test@example.com', password: 'correctPassword' });
       expect(response.statusCode).toBe(200);
       expect(response.body.accessToken).toBeTruthy();
       expect(response.body.user.email).toBe('test@example.com');
     });
     it('should not return password', async () => {
-      const user = new User({ email: 'test@example.com', password: bcrypt.hashSync('correctPassword', 10) });
+      const user = new User({
+        email: 'test@example.com',
+        password: bcrypt.hashSync('correctPassword', 10),
+      });
       await user.save();
-      const response = await request(app).post('/api/users/login').send({ email: 'test@example.com', password: 'correctPassword' });
+      const response = await request(app)
+        .post('/api/user/login')
+        .send({ email: 'test@example.com', password: 'correctPassword' });
       expect(response.statusCode).toBe(200);
       expect(response.body.accessToken).toBeTruthy();
       expect(response.body.password).toBe(undefined);
@@ -90,30 +97,39 @@ describe('User Routes', () => {
       await User.deleteMany({});
     });
     it('should fail with invalid data', async () => {
-      const response = await request(app).post('/api/users/signup').send({ email: 'invalid', password: 'short' });
+      const response = await request(app)
+        .post('/api/user/signup')
+        .send({ email: 'invalid', password: 'short' });
       expect(response.statusCode).toBe(400);
       expect(response.body.name).toBe('ZodValidationError');
     });
 
     it('should succeed with valid data', async () => {
-      const response = await request(app).post('/api/users/signup').send({ email: 'newuser@example.com', password: 'newpassword123' });
+      const response = await request(app)
+        .post('/api/user/signup')
+        .send({ email: 'newuser@example.com', password: 'newpassword123' });
       expect(response.statusCode).toBe(200);
       expect(response.body.user.email).toBe('newuser@example.com');
     });
     it('should not return password', async () => {
-      const response = await request(app).post('/api/users/signup').send({ email: 'newuser2@example.com', password: 'newpassword123' });
+      const response = await request(app)
+        .post('/api/user/signup')
+        .send({ email: 'newuser2@example.com', password: 'newpassword123' });
       expect(response.statusCode).toBe(200);
       expect(response.body.password).toBe(undefined);
     });
 
     it('should fail with existing email', async () => {
-      const user = new User({ email: 'existing@example.com', password: bcrypt.hashSync('password123', 10) });
+      const user = new User({
+        email: 'existing@example.com',
+        password: bcrypt.hashSync('password123', 10),
+      });
       await user.save();
-      const response = await request(app).post('/api/users/signup').send({ email: 'existing@example.com', password: 'password123' });
+      const response = await request(app)
+        .post('/api/user/signup')
+        .send({ email: 'existing@example.com', password: 'password123' });
       expect(response.statusCode).toBe(409);
-      expect(response.body.message).toBe("existing@example.com already exists");
-
+      expect(response.body.message).toBe('existing@example.com already exists');
     });
   });
 });
-
